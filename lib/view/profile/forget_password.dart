fe11e94c57e819a3b75c02c9e4c4e6f693b3f5c3
daisuke0316 <dshengtian6@gmail.com>
2022-11-08 21:58:52 +0900

    forget_passwordを編集

--- lib/view/profile/forget_password.dart
+++ lib/view/profile/forget_password.dart
@@ -15,97 +15,99 @@ class ForgetPassword extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                            "/", ModalRoute.withName('profile_page'));
-                        //Navigator.of(context).pop();
-                      },
-                      child: const Icon(
-                        Icons.arrow_back_ios_new,
-                        size: 30,
-                        color: Colors.white,
+      body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        reverse: true,
+        child: SafeArea(
+          child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      TextButton(
+                        onPressed: () {
+                          //Navigator.of(context).pushNamedAndRemoveUntil(
+                          // "/", ModalRoute.withName('login_page'));
+
+                          Navigator.of(context).pop();
+                        },
+                        child: const Icon(
+                          Icons.arrow_back_ios_new,
+                          size: 30,
+                          color: Colors.white,
+                        ),
                       ),
+                    ],
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.07,
+                        right: MediaQuery.of(context).size.width * 0.4),
+                    child: const purpleBlur(),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  ShaderMask(
+                    child: const Text(
+                      'パスワード再設定',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'acme'),
                     ),
-                  ],
-                ),
-                Container(
-                  margin: EdgeInsets.only(
-                      top: MediaQuery.of(context).size.height * 0.07,
-                      right: MediaQuery.of(context).size.width * 0.4),
-                  child: const purpleBlur(),
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                ShaderMask(
-                  child: Text(
-                    'パスワード再設定',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: 'acme'),
-                  ),
-                  shaderCallback: (Rect rect) {
-                    return LinearGradient(
-                            begin: Alignment.centerLeft,
-                            end: Alignment.centerRight,
-                            colors: [Color(0xffbfc3ff), Color(0xfff7a5ff)])
-                        .createShader(rect);
-                  },
-                ),
-                const SizedBox(
-                  height: 70,
-                ),
-                const Text(
-                  'メールアドレスを入力してください',
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
-                      fontSize: 15, color: Colors.white, fontFamily: 'acme'),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: MediaQuery.of(context).size.width * 0.05,
-                    left: MediaQuery.of(context).size.width * 0.05,
-                  ),
-                  child: TextFormField(
-                    controller: emailController,
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'メールアドレスを入力してください';
-                      } else if (value.isValidEmail() == false) {
-                        return 'invalid email';
-                      }
-                      // else if (value.isValidEmail() == true) {
-                      //   return null;
-                      // }
-                      return null;
+                    shaderCallback: (Rect rect) {
+                      return const LinearGradient(
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                              colors: [Color(0xffbfc3ff), Color(0xfff7a5ff)])
+                          .createShader(rect);
                     },
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: textFormDecoration.copyWith(
-                        labelText: 'Email Address', hintText: 'メールアドレス'),
                   ),
-                ),
-                const SizedBox(height: 300),
-                SizedBox(
-                  width: 400,
-                  height: 50,
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.1,
                     ),
-                    child: const SignUpButton(name: '送信する'),
+                    child: const Text(
+                      'パスワード再設定ページをURLを記載したメールを送信します \nログインIDとして利用中のメールアドレスを入力してください。',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                          fontFamily: 'acme'),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: MediaQuery.of(context).size.width * 0.05,
+                      left: MediaQuery.of(context).size.width * 0.05,
+                      top: MediaQuery.of(context).size.height * 0.01,
+                    ),
+                    child: TextFormField(
+                      controller: emailController,
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'メールアドレスを入力してください';
+                        } else if (value.isValidEmail() == false) {
+                          return 'invalid email';
+                        }
+                        // else if (value.isValidEmail() == true) {
+                        //   return null;
+                        // }
+                        return null;
+                      },
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: textFormDecoration.copyWith(
+                          prefixIcon: Icon(Icons.mail), hintText: 'メールアドレス'),
+                    ),
+                  ),
+                  const SizedBox(height: 60),
+                  TextButton(
                     onPressed: () async {
                       if (_formKey.currentState!.validate()) {
                         AuthRepo.sendPasswordResetEmail(emailController.text);
@@ -113,10 +115,15 @@ class ForgetPassword extends HookConsumerWidget {
                         print('form not valid');
                       }
                     },
+                    child: const SizedBox(
+                      width: 300,
+                      height: 50,
+                      child: SignUpButton(name: '送信する'),
+                    ),
                   ),
-                )
-              ],
-            )),
+                ],
+              )),
+        ),
       ),
     );
   }
